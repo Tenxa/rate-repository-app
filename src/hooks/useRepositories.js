e00ca415620a6b17c8badd029eb48d1fa6148ff1
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2021-09-28 14:13:37 +0300

    10.11

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,27 @@
+//import React from 'react';
+import { useQuery } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+
+  const { data, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const fetchRepositories = () => {
+    if (data) {
+      setRepositories(data.repositories);
+    }
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, [data]);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
